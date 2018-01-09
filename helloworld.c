b63b8da9aee2359b68984b9bb8390a430524119a
wb-lys243745 <wb-lys243745@autonavi.com>
2018-01-09 18:19:40 +0800

    format

--- helloworld.c
+++ helloworld.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 #include "helloworld.h"
 
-int Mfree(){  
-    int l_stack;//stack 4bytes  
-    char l_stack_1[100];//stack 100bytes  
-    char * l_stack_2 = "this string save to ro data segment,but l_stack_2 save to stack(4bytes)";  
-    char * l_stack_3;//l_stack_3 save to stack(4bytes)  
-    char  l_stack_4[] = {"this string save to ro data segment,but l_stack_4 save to stack(4bytes)"};  
-  
-    static char l_rw[] = {"save to rw data segment"};  
-    static char l_bss[100];//save to bss segment  
-    static int l_rw_1 = 1;//save to rw data segment  
-  
-    char * m_p = (char *) malloc(100*sizeof(char));//save to heap  
-    free(m_p);  
-      
-    return 0;   
+int Mfree() {
+    int l_stack;//stack 4bytes
+    char l_stack_1[100];//stack 100bytes
+    char * l_stack_2 = "this string save to ro data segment,but l_stack_2 save to stack(4bytes)";
+    char * l_stack_3;//l_stack_3 save to stack(4bytes)
+    char  l_stack_4[] = {"this string save to ro data segment,but l_stack_4 save to stack(4bytes)"};
+
+    static char l_rw[] = {"save to rw data segment"};
+    static char l_bss[100];//save to bss segment
+    static int l_rw_1 = 1;//save to rw data segment
+
+    char * m_p = (char *) malloc(100*sizeof(char));//save to heap
+    free(m_p);
+
+    return 0;
 }
-int demo(){
+int demo() {
     PTR_TO_ARR parr = str;
     PTR_TO_FUNC pfunc = _max;
     int i;
-   
+
     printf("_max: %d\n", (*pfunc)(10, 20));
-    for(i=0; i<3; i++){
+    for(i=0; i<3; i++) {
         printf("str[%d]: %#X==>%#X-->len%d  %s\n", i, parr, parr+i, sizeof(*(parr+i)), *(parr+i));
     }
     int a=2, b=3;
@@ -34,46 +34,47 @@ int demo(){
     printf("%d-%d", a, b);
     return 0;
 }
-long factorial(int n){
+long factorial(int n) {
     long result;
-    if(n==0 || n==1){
+    if(n==0 || n==1) {
         result = 1;
-    }else{
+    } else {
         result = factorial(n-1) * n;  // 递归调用
     }
     return result;
 }
-int sum(){
+int sum() {
     int i, sum=0;
-    for(i=1; i<=100; i++){
+    for(i=1; i<=100; i++) {
         sum+=i;
     }
     return sum;
 }
-int _max(int a, int b){
+int _max(int a, int b) {
     return a>b ? a : b;
 }
 
-int binary_search(const int A[], int X, int N){
-	/*
-		A param  sorted array
-		X param  X in A 
-		N param  A lenght  
-	*/
-	int Low, Mid, High;
-	Low = 0; High = N - 1;
-	while(Low <= High){
-		Mid = (Low + High)/2;
-		if(A[Mid] < X){
-			Low = Mid + 1;
-		}else if(A[Mid] > X){
-			High = Mid -1;
-		}else{
-			return Mid;
-		}
-	}
-	return -1;
-	
+int binary_search(const int A[], int X, int N) {
+    /*
+    	A param  sorted array
+    	X param  X in A
+    	N param  A lenght
+    */
+    int Low, Mid, High;
+    Low = 0;
+    High = N - 1;
+    while(Low <= High) {
+        Mid = (Low + High)/2;
+        if(A[Mid] < X) {
+            Low = Mid + 1;
+        } else if(A[Mid] > X) {
+            High = Mid -1;
+        } else {
+            return Mid;
+        }
+    }
+    return -1;
+
 }
 
 /*
@@ -82,10 +83,9 @@ int binary_search(const int A[], int X, int N){
 当b为0时，两数的最大公约数即为a
 getchar()会接受前一个scanf的回车符
 */
-unsigned int Gcd(unsigned int M, unsigned int N){
+unsigned int Gcd(unsigned int M, unsigned int N) {
     unsigned int Rem;
-    while(N > 0)
-    {
+    while(N > 0) {
         Rem = M % N;
         M = N;
         N = Rem;
@@ -93,29 +93,29 @@ unsigned int Gcd(unsigned int M, unsigned int N){
     return M;
 }
 
-int IsEven(unsigned int N){
-	int flag = 0;
-	if( N%2==0 ){
-		flag = 1;
-	}
-	return flag;
+int IsEven(unsigned int N) {
+    int flag = 0;
+    if( N%2==0 ) {
+        flag = 1;
+    }
+    return flag;
 }
 
 /*
-	平方算法 
+	平方算法
 */
-long int Pow(long int X, unsigned int N){
-	if(N==0){
-		return 1;
-	}
-	if(N==1){
-		return X;
-	}
-	if( IsEven(N) ){
-		return Pow(X*X, N/2);
-	}else{
-		return Pow(X*X, N/2)*X;
-	}
+long int Pow(long int X, unsigned int N) {
+    if(N==0) {
+        return 1;
+    }
+    if(N==1) {
+        return X;
+    }
+    if( IsEven(N) ) {
+        return Pow(X*X, N/2);
+    } else {
+        return Pow(X*X, N/2)*X;
+    }
 }
 
 
--- helloworld.h
+++ helloworld.h
@@ -1,11 +1,11 @@
 
-long factorial(int n);   //预处理函数 
+long factorial(int n);   //预处理函数
 int sum();
 int _max(int a, int b);
 int Mfree();
 int demo();
 
-typedef char (*PTR_TO_ARR)[30];   //别名 
+typedef char (*PTR_TO_ARR)[30];   //别名
 typedef int (*PTR_TO_FUNC)(int, int);
 
 const char str[3][30] = {
@@ -13,17 +13,17 @@ const char str[3][30] = {
     "C语言中文网",
     "C-Language"
 };
-const char g_ro[] = {"save to ro data segment"};  
-const char g_rw[] = {"save to rw data segment"};  
-int g_bss[100];//save to bss segment  
+const char g_ro[] = {"save to ro data segment"};
+const char g_rw[] = {"save to rw data segment"};
+int g_bss[100];//save to bss segment
 
 // binary_search
 int binary_search(const int Array[], int X, int N);
 // 欧几里德算法
 unsigned int Gcd(unsigned int M,unsigned int N);
 // 计算X^N的算法是使用N-1
-int IsEven(unsigned int N); // 是否为奇数，偶数 
-long int Pow(long int X, unsigned int N); 
+int IsEven(unsigned int N); // 是否为奇数，偶数
+long int Pow(long int X, unsigned int N);
 
- 
+
 
